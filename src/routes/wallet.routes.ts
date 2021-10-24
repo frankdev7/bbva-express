47dc827c7727acad8e2763844f44ec3a5b51b058
Jhonatan Iberico <jhonatanibericom@gmail.com>
2021-10-23 22:45:23 -0500

    cambios

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,12 +1,58 @@
 // path - ruta : api/users
 import { Router } from 'express';
 import { createToken, getAll, update, getByDocument, getBBTC, transferBuy, transferSell } from '../controllers/wallet.controller';
+const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+
 
 const router = Router();
 
+// const swaggerOptions = {
+//     'swaggerDefinitions': {
+//         'info': {
+//             'titles': 'Wallet Api',
+//             'description': 'Wallert info API',
+//             'contact': {
+//                 'name': 'dev'
+//             },
+//             'servers': ["http://localhost:5000/api/wallet/"]
+//         }
+//     },
+//     'apis': ['wallet.routes.ts']
+// }
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Ya está papo",
+        version: "0.1.0",
+        description:
+          "Acá tienes que probar papo",
+      },
+      servers: [
+        {
+          url: "http://localhost:5000/api/wallet/",
+        },
+      ],
+    },
+    apis: ['./*ts'],
+  };
+
+const swaggerDocs = swaggerJsDoc(options);
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 // router.post('/', createToken);
+/**
+ * @swagger
+ * /
+ * get:
+ *      description: use this method to get all the persons
+ *      response:
+ *      '200':
+ *          description: A succesful response
+ */
 router.get('/', getAll);
 router.patch('/', update)
 router.get('/document/:id', getByDocument)
